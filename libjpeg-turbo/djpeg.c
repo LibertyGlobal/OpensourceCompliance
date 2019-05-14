1739ae8a036cf8ac80fa51b0dfaa568ecf7550f3
Elizabeth 'pidge' Flanagan <pidge@toganlabs.com>
2019-05-14 12:40:37 +0000

    Committing dcx960-release-eos-20190410125705_rel-69
    Committing for open source release compliance
    
    Signed-off-by: Elizabeth 'pidge' Flanagan <pidge@toganlabs.com>

---
+++ libjpeg-turbo/djpeg.c
@@ -0,0 +1,9 @@
+/*
+ * djpeg.c
+ *
+ * This file was part of the Independent JPEG Group's software:
+ * Copyright (C) 1991-1997, Thomas G. Lane.
+ * libjpeg-turbo Modifications:
+ * Copyright (C) 2010-2011, 2013-2014, D. R. Commander.
+ * For conditions of distribution and use, see the accompanying README file.
+ *
