1b40304539a2447e3e4f7ffbb4fe584693fd2bed
Katrina Prosise <igorina@toganlabs.com>
2020-05-22 15:23:49 -0400

    Committing apollo-release-apollo-20200521210001_rel-95
    Committing for open source release compliance
    
    Signed-off-by: Katrina Prosise <igorina@toganlabs.com>

--- c-ares/ares_init.c
+++
@@ -1,3 +0,0 @@
-
-/* Copyright 1998 by the Massachusetts Institute of Technology.
- * Copyright (C) 2007-2013 by Daniel Stenberg
--- wireless-tools/sample_enc.c
+++
@@ -1,4 +0,0 @@
-/* Note : this particular snipset of code is available under
- * the LGPL, MPL or BSD license (at your choice).
- * Jean II
- */
