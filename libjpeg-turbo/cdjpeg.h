d0f01b7cb86bac692af8e90b361f3020739dc9c7
Katrina Prosise <igorina@toganlabs.com>
2020-08-10 10:25:58 -0400

    Committing smtg7401-release-selene-20200807210000_rel_verity-99
    Committing for open source release compliance
    
    Signed-off-by: Katrina Prosise <igorina@toganlabs.com>

---
+++ libjpeg-turbo/cdjpeg.h
@@ -0,0 +1,12 @@
+/*
+ * cdjpeg.h
+ *
+ * This file was part of the Independent JPEG Group's software:
+ * Copyright (C) 1994-1997, Thomas G. Lane.
+ * It was modified by The libjpeg-turbo Project to include only code relevant
+ * to libjpeg-turbo.
+ * For conditions of distribution and use, see the accompanying README file.
+ *
+ * This file contains common declarations for the sample applications
+ * cjpeg and djpeg.  It is NOT used by the core JPEG library.
+ */
